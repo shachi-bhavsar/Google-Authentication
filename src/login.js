0f109b83f47b03d950311a45378150cdf745451e
shachi <bhavsarshachi@gmail.com>
2018-02-13 15:54:27 +0530

    Update login.js

--- src/login.js
+++ src/login.js
@@ -4,22 +4,51 @@ import {GoogleLogout} from 'react-google-oauth'
     
 export default class Login extends React.Component { 
 
+    constructor(props){
+        super(props)
+        this.state = {
+            tag : ''
+        }
+    }
     onSignIn(userProfile, accessToken) {
         console.log(userProfile.email)
+        if(userProfile)
+        {
+            this.setState(
+                {
+                    tag : userProfile.email
+                }
+            ); 
+        }
     }
     
     signOut() {
-        this.googleAuth.signOut();
+        this.setState(
+            {
+                tag : ''
+            }
+        );
+        this.googleAuth.signOut();         
     }
 
     render() {
         return (
             <div>
-            <GoogleSignIn clientId="YOUR_CLIENT_ID"
-                        ref={g => this.googleAuth = g}
-                        onSuccess={this.onSignIn.bind(this)}
-            />
-            <div><GoogleLogout onClick={this.signOut.bind(this)}/></div> 
+                <div>
+                <GoogleSignIn clientId="260101237603-n4a0hkmaccsdhb9i67r38g2f6ebc87gn.apps.googleusercontent.com"
+                            ref={g => this.googleAuth = g}
+                            onSuccess={this.onSignIn.bind(this)}
+                />
+                </div>
+                <div>
+                    {this.state.tag}
+                </div>
+                <div>
+                    <GoogleLogout
+                        buttonText="Logout"
+                        onLogoutSuccess={this.signOut.bind(this)}
+                    />
+                </div>
             </div>
         )   
     }
