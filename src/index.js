62cf4116286bbe2949723eadf2a0225077b70cd6
shachi <bhavsarshachi@gmail.com>
2018-02-14 11:41:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,16 +9,11 @@ import registerServiceWorker from './registerServiceWorker';
 const responseGoogle = (response) => {
     console.log(response);    
 }
-
-const onFailure = (response) => {
-    console.log(response);
-}
-
 ReactDOM.render(<GoogleAPI clientId="260101237603-n4a0hkmaccsdhb9i67r38g2f6ebc87gn.apps.googleusercontent.com"
     onUpdateSigninStatus={responseGoogle}
-    onInitFailure={onFailure} >
+    onInitFailure={(response) => {console.log(response)}} >
     <div>
-                <div><Login/></div>
+        <div><Login/></div>
     </div>
     </GoogleAPI>, document.getElementById('root'));
 registerServiceWorker();
